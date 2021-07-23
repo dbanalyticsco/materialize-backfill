dc09fd98c776c9dcc1e6038cf684ccad132d6936
Dylan Baker <dylan@dbanalytics.co>
2021-07-23 14:16:04 +0100

    change logic to a macro

--- models/marts/frankenmovies/fct_tracks.sql
+++ models/marts/frankenmovies/fct_tracks.sql
@@ -1,13 +1,19 @@
 {{ config(materialized='table') }}
 
-with unioned as (
+-- with unioned as (
 
-    select * from {{ ref('stg_frankenmovies__tracks_s3') }}
-    where event_timestamp < (select min(event_timestamp) from {{ ref('stg_frankenmovies__tracks_kinesis') }})
-    union all
-    select * from {{ ref('stg_frankenmovies__tracks_kinesis') }}
+--     select *, 's3' as source from {{ ref('stg_frankenmovies__tracks_s3') }}
+--     where event_timestamp < (select min(event_timestamp) from {{ ref('stg_frankenmovies__tracks_kinesis') }})
+--     union all
+--     select *, 'kinesis' as source from {{ ref('stg_frankenmovies__tracks_kinesis') }}
 
-)
+-- )
 
-select *
-from unioned
+-- select *
+-- from unioned
+
+{{ union_relations(
+    s3_relation=ref('stg_frankenmovies__tracks_s3'),
+    kinesis_relation=ref('stg_frankenmovies__tracks_kinesis'),
+    timestamp_column='event_timestamp'
+) }}
